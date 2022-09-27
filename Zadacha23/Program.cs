d1e9e0daed3547784cef15aa1ad944f2a09a8159
Arina M <apelsinka7.ru@yandex.ru>
2022-09-28 00:21:36 +0300

    решение задачи 23

---
+++ Zadacha23/Program.cs
@@ -0,0 +1,18 @@
+﻿int N()
+{
+    Console.WriteLine("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());     
+}
+
+void GetPrint(int N)
+{    
+    int count = 1;
+    Console.WriteLine($"Таблица кубов чисел от 1 до числа {N}:  ");
+    while (count <= N)
+{ 
+   Console.Write(Math.Pow((count),3) + " ");
+   count++;
+}
+}
+
+GetPrint(N());
