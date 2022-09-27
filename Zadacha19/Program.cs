7f218dc0b9f06a2a814da08ccdf2057bb349ea3e
Arina M <apelsinka7.ru@yandex.ru>
2022-09-27 22:38:00 +0300

    решение задачи 19

---
+++ Zadacha19/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = 0;
+while ( (n < 10000) || (n > 100000) )
+{
+   Console.Write("Пятизначное число: ");
+   n = Convert.ToInt32(Console.ReadLine());
+}
+if (n / 10000 == n % 10 && n % 10000 / 1000 == n % 100 / 10)
+   Console.WriteLine("Число {0} является палиндромом!", n);
+else
+   Console.WriteLine("Число {0} не является палиндромом!", n);
